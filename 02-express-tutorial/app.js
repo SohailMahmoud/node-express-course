efc78d617c070b4469b3efeda482e90f5075fe00
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-02-06 17:08:51 +0300

    Postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,60 @@
-const express = require("express")
-const app = express()
-const { products } = require("./data")
+const { people } = require("./data");
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1><a href="/api/products">products</a>`)
-})
+app.use(express.static("./methods-public"));
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const {id, name, image} = product
-    return {id, name, image}
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ sucess: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "Please write donw a name" });
+  }
+  res.status(201).json({ sucess: true, person: name });
+});
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "Please write donw a name" });
+  }
+  res.status(201).json({ sucess: true, data: [...people, name] });
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.name) {
+    return res.status(200).send(`Hello there, ${req.body.name}`);
+  }
+
+  res.status(401).send("Please type down your name");
+});
+
+app.delete("/api/postman/people/:id", (req, res) => {
+  const {id} = req.params
+  const updatedPerson = people.find(person => person.id === Number(id))
+  if (!updatedPerson) {
+    return res
+      .status(404)
+      .json({ sucess: false, msg: `${id} not found` });
+  }
+
+  const newArray = people.filter((person) => {
+    return person.id !== Number(id)
   })
-  res.json(newProducts)
+
+  return res.status(200).json({success: true, data: newArray})
+
 })
 
 app.listen(5000, () => {
-  console.log("Server is alive");
-})
-
+  console.log("Server is listening on port 5000....");
+});
