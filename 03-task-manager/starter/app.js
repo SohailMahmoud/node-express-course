818aff114be8804f05756a171f3ffae7f1ccd748
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-02-08 18:06:04 +0300

    Feat: connect to Mango Atlas db

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,28 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect")
+require("dotenv").config()
+
+// add static assests
+app.use(express.static("./public"));
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+
+// setting the port and connect to the db
+const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(3000, () => {
+      console.log(`Server is alive at port ${port}...`);
+    });
+  } catch(error) {
+    console.log(error);
+  }
+}
+start()
