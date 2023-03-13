4bb01443d38858ae1809580f45cdb04d844255b0
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-03-13 18:04:33 +0300

    connect to database for the store api project

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+require("dotenv").config()
+
+// async errors 
+
+const express = require("express")
+const app = express()
+
+const connectDB = require("./db/connect")
+const productRouter = require("./routes/products")
+
+const NotFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+
+// middleware 
+app.use(express.json())
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use("/api/v1/products", productRouter)
+
+// products route 
+
+app.use(NotFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log("Server is alive..."))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
